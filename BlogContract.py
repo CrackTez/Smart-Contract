ed07f0725c97a5c8247fc3b9202c9e9794de806a
Ankush Singh <ankush4singh@gmail.com>
2022-08-08 12:05:48 +0530

    copy storage from another contract

--- BlogContract.py
+++ BlogContract.py
@@ -1,6 +1,6 @@
 import smartpy as sp
 
-class PostLedger: 
+class PostLedger:
     def get_type():
         return sp.TRecord(
                 author = sp.TAddress,
@@ -16,10 +16,20 @@ class PostLedger:
 class Contract(sp.Contract):
     def __init__(self):
         # Storage
-        self.init(
+        self.init_storage(
+            admin = sp.address("tz1VLj6WqWLeFjn4BWdoScpN752qSEyqwXFV"),
             posts = sp.big_map(l = {}, tkey = sp.TNat, tvalue = PostLedger.get_type()),
             next_count = sp.nat(0)
         )
+
+    @sp.entry_point
+    def set_storage_from(self, addr):
+        sp.set_type(addr, sp.TAddress)
+        sp.verify(sp.sender == self.data.admin, "UNAUTHORISED")
+        NextCount = sp.view("GetNextCount", addr, sp.nat(0), t=sp.TNat)
+        Posts = sp.view("GetPosts", addr, sp.nat(0), t=sp.TBigMap(sp.TNat,PostLedger.get_type()))
+        self.data.next_count = NextCount.open_some()
+        self.data.posts = Posts.open_some()
 
     @sp.entry_point
     def create_post(self, ipfs_url, thumbnail_url, title, fr_goal):
@@ -50,12 +60,22 @@ class Contract(sp.Contract):
 
         contributers = post.contributers
 
-        contribute_amt = sp.local("contribute_amt", sp.amount)
+        contribute_amt = sp.local("contribute_amt", sp.amount - sp.mutez(2000))
+        sp.send(post.author,contribute_amt.value)
+        post.fundraised += contribute_amt.value
         sp.if contributers.contains(sp.sender):
             contribute_amt.value += contributers[sp.sender]
-        sp.send(post.author,sp.amount)
-        post.fundraised += sp.amount
         contributers[sp.sender] = contribute_amt.value
+
+    @sp.onchain_view(name = "GetNextCount")
+    def get_next_count(self, x):
+        sp.set_type(x, sp.TNat)
+        sp.result(self.data.next_count)
+
+    @sp.onchain_view(name = "GetPosts")
+    def get_posts(self, x):
+        sp.set_type(x, sp.TNat)
+        sp.result(self.data.posts)
 
         
 
@@ -66,19 +86,30 @@ def main():
     cont = Contract()
     scenario += cont
 
-    weeblet = sp.test_account ("weeblet")
+    weeblet = sp.address("tz1VLj6WqWLeFjn4BWdoScpN752qSEyqwXFV")
     other = sp.test_account ("oth")
     other1 = sp.test_account ("oth1")
 
     cont.create_post(
         ipfs_url="ok",
         thumbnail_url="ok",
-        title="Demo Post",
+        title="Demo Post, ghgh",
         fr_goal = sp.tez(69)
     ).run(sender = weeblet)
 
-    cont.send_tip(0).run(sender=other1.address, amount=sp.mutez(10000))
-    cont.send_tip(0).run(sender=other.address, amount=sp.tez(1))
-    cont.send_tip(0).run(valid=False, sender=weeblet.address, amount=sp.tez(2))
-    cont.send_tip(0).run(sender=other.address, amount=sp.tez(2))
+    cont.send_tip(0).run(sender=other1, amount=sp.mutez(10000))
+    cont.send_tip(0).run(sender=other, amount=sp.tez(1))
+    cont.send_tip(0).run(sender=weeblet, amount=sp.tez(2), valid=False)
+    cont.send_tip(0).run(sender=other, amount=sp.tez(2))
+
+    v = sp.view("GetNextCount", cont.address, sp.nat(0), t=sp.TNat)
+    scenario.show(v)
+    v = sp.view("GetPosts", cont.address, sp.nat(0), t=sp.TBigMap(sp.TNat,PostLedger.get_type()))
+    scenario.show(v)
+
+    c2 = Contract()
+    scenario += c2
+
+    c2.set_storage_from(cont.address).run(sender=weeblet)
+    c2.set_storage_from(cont.address).run(sender=other, valid=False)
 
